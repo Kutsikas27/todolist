b973066325541b45ea1d2df58bf2d7a74605dcfd
kutsikas27 <mardus80@gmail.com>
2024-01-29 10:15:00 +0200

    add css and js

---
+++ index.js
@@ -0,0 +1,48 @@
+// @ts-nocheck
+const inputText = document.getElementById("userInput");
+let itemsArray = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
+console.log(itemsArray);
+const addToList = () => {
+  itemsArray.push(inputText.value);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  addTask(inputText.value);
+  inputText.value = "";
+};
+
+inputText.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addToList();
+  }
+});
+
+const addTask = (text) => {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  li.textContent = text;
+  li.classList.add("list-group-item", "list-group-item-success");
+
+  btn.classList.add("btn", "btn-danger", "bi", "bi-trash");
+  document.getElementById("list").appendChild(li).appendChild(btn);
+  btn.onclick = function () {
+    removeTask(text);
+
+    document.getElementById("list").removeChild(li);
+  };
+};
+
+const clearAllTasks = () => {
+  localStorage.clear();
+  document.getElementById("list").innerHTML = "";
+  itemsArray = [];
+};
+
+const removeTask = (item) => {
+  itemsArray.splice(itemsArray.indexOf(item), 1);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+
+  console.log(localStorage);
+  console.log(itemsArray);
+};
+itemsArray.forEach(addTask);
